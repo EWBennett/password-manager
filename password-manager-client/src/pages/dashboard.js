56b64c72797628159b45a36132c3bf2fcf907931
Edward Bennett <fm014385@student.reading.ac.uk>
2021-06-11 21:43:27 +0100

    Continued work on dashboard and endpoints

--- password-manager-client/src/pages/dashboard.js
+++ password-manager-client/src/pages/dashboard.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
-import { AppBar, Drawer, IconButton, Toolbar, Typography, Divider } from "@material-ui/core";
+import {
+  AppBar,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+  Divider,
+  Container,
+} from "@material-ui/core";
+import { MenuIcon, ChevronLeftIcon } from "@material-ui/icons";
 
 class dashboard extends Component {
   constructor() {
@@ -7,6 +16,14 @@ class dashboard extends Component {
   }
 
   render() {
+    const [open, setOpen] = React.useState(true);
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
+
     return (
       <div>
         <AppBar position="absolute">
@@ -15,21 +32,36 @@ class dashboard extends Component {
               edge="start"
               color="inherit"
               aria-label="open drawer"
-              //onClick={handleDrawerOpen}
-            ></IconButton>
+              onClick={handleDrawerOpen}
+            >
+              <MenuIcon />
+            </IconButton>
             <Typography component="h1" variant="h6" color="inherit" noWrap>
               Your Dashboard
             </Typography>
           </Toolbar>
         </AppBar>
-        <Drawer variant="permanent">
+        <Drawer variant="permanent" open={open}>
           <div className="toolbarIcon">
-            <IconButton
-            //onclick={handleDrawerClose}
-            ></IconButton>
+            <IconButton onclick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
           </div>
           <Divider />
+          <List>
+            
+          </List>
         </Drawer>
+        {/* Body of the page*/}
+        <main>
+          <Container maxWidth="lg">
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper>
+
+              </Paper>
+            </Grid>
+          </Container>
+        </main>
       </div>
     );
   }
